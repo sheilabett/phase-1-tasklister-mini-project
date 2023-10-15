d90da2c5bad29a859950e2dcc033db00e9ada1b8
sheilabett <sheilahbett@gmail.com>
2023-10-15 11:53:26 +0300

    Complete to do list assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+  const taskInput = document.getElementById('new-task-description');
+  const taskDescription = taskInput.value;
+  
+  const taskList = document.getElementById('tasks');
+    const newTask = document.createElement('li');
+    newTask.textContent = taskDescription;
+
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+ });
 });
